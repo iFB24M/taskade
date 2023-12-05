a7b5bfd08a216c64303aa9e06e1393fc6f9d95cc
FB24M <103760918+iFB24M@users.noreply.github.com>
2023-12-05 22:31:43 +0400

    Add files via upload

--- src/blocks/Sidebar/Sidebar.component.tsx
+++ src/blocks/Sidebar/Sidebar.component.tsx
@@ -2,6 +2,17 @@ import styles from './Sidebar.module.scss'
 import { Icon } from '../../ui/components/Icon/Icon.component'
 import { Link } from 'react-router-dom'
 
+const changeTheme = () => {
+	if (localStorage.getItem('color-theme') === 'dark') {
+		localStorage.setItem('color-theme', 'light')
+	}
+	else {
+		localStorage.setItem('color-theme', 'dark')
+	}
+
+	location.reload()
+}
+
 const Sidebar = () => {
 	return (
 		<div className={styles.sidebar}>
@@ -9,6 +20,9 @@ const Sidebar = () => {
 				<Icon icon="account_circle" /> Имя пользователя
 			</div>
 			<div className={styles.menu}>
+				<div className={styles.item} onClick={() => changeTheme()}>
+					<Icon icon={localStorage.getItem('color-theme') === 'dark' ? 'dark_mode' : 'light_mode'} /> {localStorage.getItem('color-theme') === 'dark' ? 'Темный режим' : 'Светлый режим'}
+				</div>
 				<Link to="/" className={styles.item}><Icon icon="home" />Главная</Link>
 				<Link to="/tasks" className={styles.item}><Icon icon="list" />Задачи</Link>
 				<div className={styles.subitem}><Icon icon="not_started" /> Не начатые</div>
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -8,6 +8,17 @@ import styles from './Layout.module.scss'
 
 
 const Layout = () => {
+	if (localStorage.getItem('color-theme') === 'dark') {
+		document.documentElement.style.setProperty('--background-color', '#333')
+		document.documentElement.style.setProperty('--additional-color', '#383838')
+		document.documentElement.style.setProperty('--foreground-color', '#fff')
+	}
+	else {
+		document.documentElement.style.setProperty('--background-color', '#fff')
+		document.documentElement.style.setProperty('--additional-color', '#f9f9f9')
+		document.documentElement.style.setProperty('--foreground-color', '#333')
+	}
+
 	return (
 		<div className="wrapper">
 			<main className={styles.main}>
