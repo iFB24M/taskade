6ef280435b8981f377c92e989e0ea2808608dd19
ifb24m <fakem1ner@yandex.ru>
2023-12-10 12:45:24 +0400

    add reactQuery fix type errors in home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { lazy } from 'react'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -7,19 +8,23 @@ const Tasks = lazy(() => import('./pages/tasks/page'))
 const Task = lazy(() => import('./pages/task/page'))
 const Layout = lazy(() => import('./pages/Layout'))
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route Component={Layout}>
-          <Route path="/tasks" element={<Tasks />}></Route>
-          <Route path="/tasks/:status" element={<Tasks />}></Route>
-          <Route path="/tasks/:status/:date" element={<Tasks />}></Route>
-          <Route path="/task/*" element={<Task />}></Route>
-          <Route path="/*" element={<Home />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={Layout}>
+            <Route path="/tasks" element={<Tasks />}></Route>
+            <Route path="/tasks/:status" element={<Tasks />}></Route>
+            <Route path="/tasks/:status/:date" element={<Tasks />}></Route>
+            <Route path="/task/*" element={<Task />}></Route>
+            <Route path="/*" element={<Home />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/tasks/page.tsx
+++ src/pages/tasks/page.tsx
@@ -1,11 +1,44 @@
 import styles from './page.module.scss'
-import { Icon } from '../../ui/components/Icon/Icon.component'
+import { Icon } from '@/ui/components/Icon/Icon.component'
 import { Link } from 'react-router-dom'
+import { FormEvent, useEffect, useState } from 'react';
+
+const addTask = (e: FormEvent<HTMLFormElement>) => {
+	e.preventDefault();
+
+	const formData = new FormData(e.target as HTMLFormElement)
+
+	const taskTitle = ((e.target as HTMLElement).querySelector('#task-title') as HTMLInputElement)?.value
+
+	fetch('http://127.0.0.1:5000/add', {
+		method: 'post',
+		mode: 'no-cors',
+		body: formData
+	})
+
+	console.log(`Добавление ${taskTitle}`)
+
+}
 
 const Tasks = () => {
+	const [data, setData] = useState<string | false>(false)
+
+	useEffect(() => {
+		const getData = async () => {
+			const response = await fetch('http://127.0.0.1:5000')
+			const json = await response.text()
+			console.log(response)
+			setData(json)
+		}
+		getData()
+	}, [])
+
+
+
 	return (
 		<div>
 			<div className={styles.categories}>
+				{data}
 				<Link to="/tasks" className={styles.category}>Работа</Link>
 				<Link to="/tasks" className={styles.category}>Дизайн</Link>
 				<Link to="/tasks" className={styles.category}>Домашние дела</Link>
@@ -14,13 +47,18 @@ const Tasks = () => {
 				<input className={styles.category} placeholder="Новая категория" name="new-category-name" />
 				<Link to="/tasks" className={`${styles.category} ${styles.iconcategory}`}><Icon icon="add_circle" /> добавить</Link>
 			</div>
+			<form className={styles.add} onSubmit={addTask}>
+				<input type="text" id="task-title" name="todo" placeholder='Добавить задачу' />
+				<button type="submit">Добавить</button>
+			</form>
 			<div className={styles.list}>
-				<Link to="/task/0" className={`${styles.task} ${styles.pending}`}>
-					<span className={styles.taskLabel}>Не начато</span>
-					<span className={styles.taskTitle}>Средняя задача</span>
-					<span className={styles.taskDate}><Icon icon="schedule" /> 12:00 - 13:00</span>
-				</Link>
-				<Link to="/task/0" className={`${styles.task} ${styles.inProcess}`}>
+				{/* {data ? data.list.map((item) =>
+					<Link to="/task/0" className={`${styles.task} ${styles.pending}`}>
+						<span className={styles.taskLabel}>Не начато</span>
+						<span className={styles.taskTitle}>Средняя задача</span>
+						<span className={styles.taskDate}><Icon icon="schedule" /> 12:00 - 13:00</span>
+					</Link>) : ''} */}
+				{/* <Link to="/task/0" className={`${styles.task} ${styles.inProcess}`}>
 					<span className={styles.taskLabel}>В процессе</span>
 					<span className={styles.taskTitle}>Легкая задача</span>
 					<span className={styles.taskDate}><Icon icon="schedule" /> 13:00 - 15:00</span>
@@ -29,7 +67,7 @@ const Tasks = () => {
 					<span className={styles.taskLabel}>Закончено</span>
 					<span className={styles.taskTitle}>Обычная задача</span>
 					<span className={styles.taskDate}><Icon icon="schedule" /> 14:00 - 16:00</span>
-				</Link>
+				</Link> */}
 			</div>
 		</div>
 	)
